26b3f389750beacda1c2887209a962f9a4cf48e1
Muhammad Ayan <professorayanxoffical@gmail.com>
2026-04-14 14:46:25 +0500

    Add Supabase configuration in config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+const supabaseUrl = "https://yudgecjcfrfhpkbzeuik.supabase.co";
+const supabaseKey = "sb_publishable_eZIaW_GFZsJanVNo6tZIzA_1-7yyQ4E";
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
